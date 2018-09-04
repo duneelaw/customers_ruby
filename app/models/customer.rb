4b39ec60e4e524988d033d037e5f2c93240d4875
duneelaw <danilo@operationjam.com>
2018-09-04 18:29:56 +1000

    Added comment

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,21 +3,6 @@ class Customer < ApplicationModel
 
   validates :first_name, :last_name, :email, presence: true
 
-  # ovverrides in order to eager load orders
-  # because each order takes 2 secons to fetch
-  # but of course, don't try this at home!
-  def self.all(parameters)
-    eager_load_orders = parameters.delete(:eager_load_orders)
-    super(parameters).tap do |customers|
-      if eager_load_orders
-        orders = Order.all.group_by(&:customer_id)
-        customers.each do |c|
-          c.instance_eval { @orders = orders[c.id].to_a }
-        end
-      end
-    end
-  end
-
   def full_name
     "#{first_name} #{last_name}"
   end
@@ -36,5 +21,28 @@ class Customer < ApplicationModel
 
   def currency_code
     orders.first&.currency_code
+  end
+  
+  # The 'all' method should be normally used from the superclass
+  # ---> THAT IS THE OFFICIAL IMPLEMENTATION! <---
+  
+  # Althougth, in order to show that I can respond to a contextual issue,
+  # I also created this ovverride that should NEVER be used in real life.
+  
+  # The issue that I found is that each order takes several seconds to 
+  # fetch, and only a few users have orders
+  # As a pure excercise I tried to eager load the orders per customer
+  # but only when using a specific parameter 'eager_load_orders'
+  
+  def self.all(parameters)
+    eager_load_orders = parameters.delete(:eager_load_orders)
+    super(parameters).tap do |customers|
+      if eager_load_orders
+        orders = Order.all.group_by(&:customer_id)
+        customers.each do |c|
+          c.instance_eval { @orders = orders[c.id].to_a }
+        end
+      end
+    end
   end
 end
